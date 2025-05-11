46ecfabb3c72d4c32c4cd5fea69499959fb4b6c9
PAIthon <209926046+rs3dh@users.noreply.github.com>
2025-05-12 00:10:34 +0900

    twitter.py

--- twitter.py
+++ twitter.py
@@ -14,7 +14,7 @@ post_content = st.text_input("Put your post here")
 if st.button("Post"):
     if post_content: #If post content is not empty
         st.success("Posted!")
-        st.session_st.posts.append(post_content) #Save in list
+        st.session_state.posts.append(post_content) #Save in list
     else: #If post content is empty
         st.warning("Please enter content to post") #Show alert
 
